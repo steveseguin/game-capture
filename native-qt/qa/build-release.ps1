b8e97a9e88ee30a35bae3d93831fb0f72ed27197
Steve Seguin <steve@seguin.email>
2026-03-01 13:36:45 -0500

    Bump version to 0.2.25

--- native-qt/qa/build-release.ps1
+++ native-qt/qa/build-release.ps1
@@ -1,7 +1,7 @@
 ﻿param(
     [string]$BuildDir = "build-review2",
     [string]$Configuration = "Release",
-    [string]$Version = "0.2.24",
+    [string]$Version = "0.2.25",
     [switch]$SkipVirusTotal = $false
 )
 
