adc106e17b250dea305579544a588b09edc2aacc
steveseguin <punkrawker@gmail.com>
2026-03-21 17:54:46 -0400

    chore: bump version to 0.2.32

--- native-qt/installer.nsi
+++ native-qt/installer.nsi
@@ -6,7 +6,7 @@
 ; General
 Name "Game Capture"
 !ifndef VERSION
-!define VERSION "0.2.30"
+!define VERSION "0.2.32"
 !endif
 !ifndef BUILD_BIN_DIR
 !define BUILD_BIN_DIR "build\bin"
--- native-qt/qa/build-release.ps1
+++ native-qt/qa/build-release.ps1
@@ -1,7 +1,7 @@
 ﻿param(
     [string]$BuildDir = "build-review2",
     [string]$Configuration = "Release",
-    [string]$Version = "0.2.30",
+    [string]$Version = "0.2.32",
     [switch]$SkipVirusTotal = $false
 )
 
